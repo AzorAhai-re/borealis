845fe19066ac666249469204012c514e2622dd98
Jamil Bousquet <jamil.bousquet@gmail.com>
2022-03-22 12:32:59 -0400

    addded OZ upgrades plugin

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -2,8 +2,23 @@
  * @type import('hardhat/config').HardhatUserConfig
  */
 import "@nomiclabs/hardhat-waffle";
+import '@nomiclabs/hardhat-ethers';
+import '@openzeppelin/hardhat-upgrades';
 import '@typechain/hardhat';
 
 export default {
-  solidity: "0.7.3",
+  solidity: {
+    compilers: [
+      {
+        version: "0.8.4",
+        settings: {}
+      }
+    ]
+  },
+  networks: {
+    hardhat: {
+      gas: 10000000,
+      gasPrice: 875000000,
+    }
+  }
 };
