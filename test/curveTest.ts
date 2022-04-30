4bb3831c80bbafe0f6c96c2950937f557101206c
Jamil Bousquet <jamil.bousquet@gmail.com>
2022-04-30 12:04:11 -0400

    added check for pegged xcd price post-discount

--- test/curveTest.ts
+++ test/curveTest.ts
@@ -25,6 +25,16 @@ describe("Bonding Curve Test", function () {
         curve = bondingCurve
     });
 
+
+    async function checkSpotPrice(){
+        const current_supply = await token.totalSupply();
+        const tokenSpotPrice = await math.toUInt(await curve.calcPricePerToken(current_supply.add(1e6)))
+        expect(Math.abs(tokenSpotPrice.sub(2700000).toNumber())
+        ).to.be.lt(1e5);
+
+        return tokenSpotPrice
+    }
+
     describe("Bonding", async () => {
         it("should be able to bond 1 token", async () => {
             await curve.grantRole(await curve.BOND_ROLE(), bonder.address);
@@ -42,6 +52,19 @@ describe("Bonding Curve Test", function () {
             ).to.be.lt(1e6,
                 `expecting the bonder's token balance to be ~= to ${expTokensOwed}`
             );
+
+        it("should stabalise the price once the threshold has been reached", async () => {
+            // mint to the threashold amount
+            // with the bond-deploy-mint already in the currSupply, this takes
+            // the curve way past the beginning of the "threshhold domain"
+            await token.connect(deployer).mint(receiver.address, 2962491838571 - 180573542300);
+            await curve.connect(bonder).approveBonding();
+            
+            await checkSpotPrice();
+            await curve.connect(bonder).bond({value: parseEther("1000")});
+            await checkSpotPrice();
+            await curve.connect(bonder).bond({value: parseEther("1000")});
+            await checkSpotPrice();
         });
     });
 
