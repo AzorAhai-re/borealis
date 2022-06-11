452c3ee407f191a01ad8cee0df6f434f37703aff
Jamil Bousquet <jamil.bousquet@gmail.com>
2022-06-11 09:04:27 -0400

    :sparkles: setting `manager` and providing `curve`

--- contracts/Map.sol
+++ contracts/Map.sol
@@ -2,25 +2,25 @@
 pragma solidity ^0.8.4;
 
 import "./interfaces/IMap.sol";
+import "./access/IAdmin.sol";
 
 contract Map is IMap {
-    IManager private immutable _manager;
-    IToken private immutable _token;
-
-    uint256 private rateLimitEpoch;
-    uint256 private rateLimitThreshold;
+    IManager private _manager;
+    IToken private _token;
 
     constructor(address manager) {
         _manager = IManager(manager);
-
         _token = IManager(manager).token();
     }
-
 
     function token() public override view returns(IToken) {
         return _token;
     }
 
+    function curve() public override view returns(IBondingCurve _curve) {
+        _curve = _manager.curve();
+        require(address(_curve) != address(0), "Map: curve not yet set");
+    }
 
     function updateToken() internal returns (bool hasUpdated){
         IToken currToken = _manager.token();
@@ -34,9 +34,14 @@ contract Map is IMap {
     }
 
     function setManager(address newManager) external override {
+        require(
+            IAdmin(address(_manager)).isGovernor(msg.sender),
+            "Map: only the governor can set the manager"
+        );
+        _setManager(IManager(newManager));
     }
 
-    function _setRateLimitThreshold(uint256 _newThreshold) internal {
-        rateLimitThreshold = _newThreshold;
+    function _setManager(IManager newManager) private {
+        _manager = newManager;
     }
 }
